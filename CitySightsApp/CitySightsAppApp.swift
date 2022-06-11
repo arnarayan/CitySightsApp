4eed8f3feb721e6e84dd920ef02deebb19e369fa
Anand.Narayan <arnarayan@gmail.com>
2022-06-10 21:45:54 -0600

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Anand Narayan on 2022-06-10.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
